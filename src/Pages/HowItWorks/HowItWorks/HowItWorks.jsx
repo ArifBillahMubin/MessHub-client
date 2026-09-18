1a5ee440aa4d29052125a9488323c50c74af1bd7
SajibSaha207 <sajibsaha.207@gmail.com>
2026-09-18 23:30:06 +0600

    add FindRightPlace section

--- src/Pages/HowItWorks/HowItWorks/HowItWorks.jsx
+++ src/Pages/HowItWorks/HowItWorks/HowItWorks.jsx
@@ -1,13 +1,17 @@
 
+import Navbar from '../../Shared/Navbar/Navbar';
 import Banner from './Banner/Banner';
 import FindMess from './FindMess/FindMess';
+import FindRightPlace from './FindRightPlace/FindRightPlace';
 
 
 const HowItWorks = () => {
     return (
         <div>
+            <Navbar></Navbar>
             <Banner></Banner>
             <FindMess></FindMess>
+            <FindRightPlace></FindRightPlace>
             
 
         </div>
